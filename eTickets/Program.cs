b9bdbb7ee28e6ebef86030b8400c70a8e1d3d72e
malizad77 <malizad77@outlook.com>
2026-01-17 18:44:12 -0400

    Reapply "MVC MVP created"
    This reverts commit c25c9b5ca74b08e239a6004074928720d32ca22e.

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,3 +1,6 @@
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace eTickets
 {
     public class Program
@@ -8,6 +11,9 @@ namespace eTickets
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
             var app = builder.Build();
 
@@ -29,6 +35,9 @@ namespace eTickets
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            // seed database
+            AppDbInitializer.Seed(app);
 
             app.Run();
         }
